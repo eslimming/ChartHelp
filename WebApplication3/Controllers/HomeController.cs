82c3fb4c0a9e41ef6117d2d710e8d9cd0ffb159b
EFSQ <EFSQ@DESKTOP-7IMBU4U>
2022-05-10 22:18:28 -0400

    Agregar archivos de proyecto.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+namespace Web_APIcharter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CharterHelp(int ancho=500, int alto=500)
+        {
+            new Chart(width: ancho, height: alto)
+            //.AddTitle("Chart for languages")
+                 .AddSeries(chartType: "radar",
+                   xValue: new[] { "ASP.NET", "HTML5", "C Language", "C++" },
+                     yValues: new[] { "90", "100", "80", "70" })
+                   .Write("bmp");
+            return null;
+        }
+    }
+}
